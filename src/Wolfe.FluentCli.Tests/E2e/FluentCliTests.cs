6dccb2493d912ab48c84be60bbd99adc9edc6618
Tom Wolfe <trwolfe13@gmail.com>
2021-10-07 15:30:36 +0100

    tests: Add one e2e test for default commands.

---
+++ src/Wolfe.FluentCli.Tests/E2e/FluentCliTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Wolfe.FluentCli.Core;
+using Wolfe.FluentCli.Tests.Mocks;
+using Xunit;
+
+namespace Wolfe.FluentCli.Tests.E2e
+{
+    public class FluentCliTests
+    {
+        [Fact]
+        public async Task DefaultCommand()
+        {
+            var wasCalled = false;
+            var provider = CreateMockProvider(context =>
+            {
+                wasCalled = true;
+            });
+
+            var cli = Cli.Build(cli => cli.WithServiceProvider(provider).WithDefault<MockCommandHandler>());
+
+            await cli.Execute("");
+
+            Assert.True(wasCalled);
+        }
+
+        private static ServiceProvider CreateMockProvider(Action<CliContext> handler) => (Type t) => new MockCommandHandler(handler);
+    }
+}
